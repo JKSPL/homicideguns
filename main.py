1b50569696e4ad60301f35589f911042e4cace16
Juliusz Straszyński <julek@straszynski.pl>
2020-11-29 13:42:40 +0100

    regression

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import learn as learn
 import pandas as pd
 from matplotlib import pyplot as plt
+from sklearn.linear_model import LinearRegression
+
 with open("homicide.txt") as file:
     homicide_df = pd.read_html(file.read())[0]
 with open("guns.txt") as file:
@@ -18,11 +21,20 @@ homicide_df = homicide_df[homicide_df['Region'] == 'Europe']
 
 all_df = homicide_df.join(guns_df).dropna()
 print(all_df.columns)
-plt.figure(figsize=(5, 5), dpi=300)
+plt.figure(figsize=(10, 10), dpi=300)
+
 ax = all_df.plot.scatter(x='Estimate of civilian firearms per 100 persons', y='Homicide Rate')
 for k, v in all_df.iterrows():
     p = (v['Estimate of civilian firearms per 100 persons'], v['Homicide Rate'])
     ax.annotate(k, p, fontsize=6)
+
+X = all_df['Estimate of civilian firearms per 100 persons'].values.reshape(-1, 1)
+Y = all_df['Homicide Rate'].values.reshape(-1, 1)
+linear_regressor = LinearRegression()  # create object for the class
+linear_regressor.fit(X, Y)
+Y_pred = linear_regressor.predict(X)
+
+plt.plot(X, Y_pred, color='red')
 plt.show()
 
 
